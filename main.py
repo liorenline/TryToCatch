275d6f26d02e50fd91849baabd45d8ea6b1e7fa1
Avtobu <lifeivanna2007@gmail.com>
2026-01-24 13:15:52 +0200

    Added start menu and logic of choosing sites

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 def start():
     text = r"""
   ______          ______      ______      __       __  
@@ -7,13 +9,38 @@ def start():
 /_/ /_/   \__, //_/  \____/\____/\__,_/\__/\___/_/ /_/ 
          /____/                                        
     """
-
     print(text)
+
+SITES = {
+    "1": {"name": "Instagram site", "path": "sites/instagram"},
+}
+HOST = "127.0.0.1"
+PORT = 8080
+def choose_site():
+    print("Choose site:")
+    for k, v in SITES.items():
+        print(f"[{k}] {v['name']}")
+    print("[q] Quit")
+
+    while True:
+        choice = input("> ").strip().lower()
+        if choice in ("q", "quit", "exit"):
+            raise SystemExit(0)
+        if choice in SITES:
+            site_dir = Path(SITES[choice]["path"]).resolve()
+            if site_dir.exists() and site_dir.is_dir():
+                return site_dir
+            print("Error")
+        else:
+            print("Error")
 
 def main():
     start()
+    choose_site()
 
 if __name__ == "__main__":
     main()
+
+
 
 
